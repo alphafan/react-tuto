e60b97304b9f86c12fd96e3c961afc332c12feb8
Yufan Zheng <yufan.zheng1@gmail.com>
2020-04-05 17:30:43 +0800

    class 03: react components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from './components/header';
+import Main from './components/main';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header />
+      <Main />
     </div>
   );
 }
